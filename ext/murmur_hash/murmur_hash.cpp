ffec789562d5b4ac9cfd4fbda80d183153e973e2
Jeff Hodges <jeff@somethingsimilar.com>
2009-01-30 05:41:44 -0800

    reindenting the murmur hash functions

--- ext/murmur_hash/murmur_hash.cpp
+++ ext/murmur_hash/murmur_hash.cpp
@@ -17,53 +17,53 @@
 
 unsigned int MurmurHash2 ( const void * key, int len, unsigned int seed )
 {
-	// 'm' and 'r' are mixing constants generated offline.
-	// They're not really 'magic', they just happen to work well.
+  // 'm' and 'r' are mixing constants generated offline.
+  // They're not really 'magic', they just happen to work well.
 
-	const unsigned int m = 0x5bd1e995;
-	const int r = 24;
+  const unsigned int m = 0x5bd1e995;
+  const int r = 24;
 
-	// Initialize the hash to a 'random' value
+  // Initialize the hash to a 'random' value
 
-	unsigned int h = seed ^ len;
+  unsigned int h = seed ^ len;
 
-	// Mix 4 bytes at a time into the hash
+  // Mix 4 bytes at a time into the hash
 
-	const unsigned char * data = (const unsigned char *)key;
+  const unsigned char * data = (const unsigned char *)key;
 
-	while(len >= 4)
-	{
-		unsigned int k = *(unsigned int *)data;
+  while(len >= 4)
+    {
+      unsigned int k = *(unsigned int *)data;
 
-		k *= m; 
-		k ^= k >> r; 
-		k *= m; 
+      k *= m; 
+      k ^= k >> r; 
+      k *= m; 
 		
-		h *= m; 
-		h ^= k;
+      h *= m; 
+      h ^= k;
 
-		data += 4;
-		len -= 4;
-	}
+      data += 4;
+      len -= 4;
+    }
 	
-	// Handle the last few bytes of the input array
+  // Handle the last few bytes of the input array
 
-	switch(len)
-	{
-	case 3: h ^= data[2] << 16;
-	case 2: h ^= data[1] << 8;
-	case 1: h ^= data[0];
-	        h *= m;
-	};
+  switch(len)
+    {
+    case 3: h ^= data[2] << 16;
+    case 2: h ^= data[1] << 8;
+    case 1: h ^= data[0];
+      h *= m;
+    };
 
-	// Do a few final mixes of the hash to ensure the last few
-	// bytes are well-incorporated.
+  // Do a few final mixes of the hash to ensure the last few
+  // bytes are well-incorporated.
 
-	h ^= h >> 13;
-	h *= m;
-	h ^= h >> 15;
+  h ^= h >> 13;
+  h *= m;
+  h ^= h >> 15;
 
-	return h;
+  return h;
 }
 
 //-----------------------------------------------------------------------------
@@ -78,130 +78,130 @@ unsigned int MurmurHash2 ( const void * key, int len, unsigned int seed )
 
 unsigned int MurmurHashAligned2 ( const void * key, int len, unsigned int seed )
 {
-	const unsigned int m = 0x5bd1e995;
-	const int r = 24;
+  const unsigned int m = 0x5bd1e995;
+  const int r = 24;
 
-	const unsigned char * data = (const unsigned char *)key;
+  const unsigned char * data = (const unsigned char *)key;
 
-	unsigned int h = seed ^ len;
+  unsigned int h = seed ^ len;
 
-	int align = (int)data & 3;
+  int align = (int)data & 3;
 
-	if(align && (len >= 4))
-	{
-		// Pre-load the temp registers
+  if(align && (len >= 4))
+    {
+      // Pre-load the temp registers
 
-		unsigned int t = 0, d = 0;
+      unsigned int t = 0, d = 0;
 
-		switch(align)
-		{
-			case 1: t |= data[2] << 16;
-			case 2: t |= data[1] << 8;
-			case 3: t |= data[0];
-		}
+      switch(align)
+        {
+        case 1: t |= data[2] << 16;
+        case 2: t |= data[1] << 8;
+        case 3: t |= data[0];
+        }
 
-		t <<= (8 * align);
+      t <<= (8 * align);
 
-		data += 4-align;
-		len -= 4-align;
+      data += 4-align;
+      len -= 4-align;
 
-		int sl = 8 * (4-align);
-		int sr = 8 * align;
+      int sl = 8 * (4-align);
+      int sr = 8 * align;
 
-		// Mix
+      // Mix
 
-		while(len >= 4)
-		{
-			d = *(unsigned int *)data;
-			t = (t >> sr) | (d << sl);
+      while(len >= 4)
+        {
+          d = *(unsigned int *)data;
+          t = (t >> sr) | (d << sl);
 
-			unsigned int k = t;
+          unsigned int k = t;
 
-			MURMUR_MIX(h,k,m);
+          MURMUR_MIX(h,k,m);
 
-			t = d;
+          t = d;
 
-			data += 4;
-			len -= 4;
-		}
+          data += 4;
+          len -= 4;
+        }
 
-		// Handle leftover data in temp registers
+      // Handle leftover data in temp registers
 
-		d = 0;
+      d = 0;
 
-		if(len >= align)
-		{
-			switch(align)
-			{
-			case 3: d |= data[2] << 16;
-			case 2: d |= data[1] << 8;
-			case 1: d |= data[0];
-			}
+      if(len >= align)
+        {
+          switch(align)
+            {
+            case 3: d |= data[2] << 16;
+            case 2: d |= data[1] << 8;
+            case 1: d |= data[0];
+            }
 
-			unsigned int k = (t >> sr) | (d << sl);
-			MURMUR_MIX(h,k,m);
+          unsigned int k = (t >> sr) | (d << sl);
+          MURMUR_MIX(h,k,m);
 
-			data += align;
-			len -= align;
+          data += align;
+          len -= align;
 
-			//----------
-			// Handle tail bytes
+          //----------
+          // Handle tail bytes
 
-			switch(len)
-			{
-			case 3: h ^= data[2] << 16;
-			case 2: h ^= data[1] << 8;
-			case 1: h ^= data[0];
-					h *= m;
-			};
-		}
-		else
-		{
-			switch(len)
-			{
-			case 3: d |= data[2] << 16;
-			case 2: d |= data[1] << 8;
-			case 1: d |= data[0];
-			case 0: h ^= (t >> sr) | (d << sl);
-					h *= m;
-			}
-		}
+          switch(len)
+            {
+            case 3: h ^= data[2] << 16;
+            case 2: h ^= data[1] << 8;
+            case 1: h ^= data[0];
+              h *= m;
+            };
+        }
+      else
+        {
+          switch(len)
+            {
+            case 3: d |= data[2] << 16;
+            case 2: d |= data[1] << 8;
+            case 1: d |= data[0];
+            case 0: h ^= (t >> sr) | (d << sl);
+              h *= m;
+            }
+        }
 
-		h ^= h >> 13;
-		h *= m;
-		h ^= h >> 15;
+      h ^= h >> 13;
+      h *= m;
+      h ^= h >> 15;
 
-		return h;
-	}
-	else
-	{
-		while(len >= 4)
-		{
-			unsigned int k = *(unsigned int *)data;
+      return h;
+    }
+  else
+    {
+      while(len >= 4)
+        {
+          unsigned int k = *(unsigned int *)data;
 
-			MURMUR_MIX(h,k,m);
+          MURMUR_MIX(h,k,m);
 
-			data += 4;
-			len -= 4;
-		}
+          data += 4;
+          len -= 4;
+        }
 
-		//----------
-		// Handle tail bytes
+      //----------
+      // Handle tail bytes
 
-		switch(len)
-		{
-		case 3: h ^= data[2] << 16;
-		case 2: h ^= data[1] << 8;
-		case 1: h ^= data[0];
-				h *= m;
-		};
+      switch(len)
+        {
+        case 3: h ^= data[2] << 16;
+        case 2: h ^= data[1] << 8;
+        case 1: h ^= data[0];
+          h *= m;
+        };
 
-		h ^= h >> 13;
-		h *= m;
-		h ^= h >> 15;
+      h ^= h >> 13;
+      h *= m;
+      h ^= h >> 15;
 
-		return h;
-	}
+      return h;
+    }
 }
 
 //-----------------------------------------------------------------------------
@@ -212,46 +212,46 @@ unsigned int MurmurHashAligned2 ( const void * key, int len, unsigned int seed )
 
 unsigned int MurmurHashNeutral2 ( const void * key, int len, unsigned int seed )
 {
-	const unsigned int m = 0x5bd1e995;
-	const int r = 24;
+  const unsigned int m = 0x5bd1e995;
+  const int r = 24;
 
-	unsigned int h = seed ^ len;
+  unsigned int h = seed ^ len;
 
-	const unsigned char * data = (const unsigned char *)key;
+  const unsigned char * data = (const unsigned char *)key;
 
-	while(len >= 4)
-	{
-		unsigned int k;
+  while(len >= 4)
+    {
+      unsigned int k;
 
-		k  = data[0];
-		k |= data[1] << 8;
-		k |= data[2] << 16;
-		k |= data[3] << 24;
+      k  = data[0];
+      k |= data[1] << 8;
+      k |= data[2] << 16;
+      k |= data[3] << 24;
 
-		k *= m; 
-		k ^= k >> r; 
-		k *= m;
+      k *= m; 
+      k ^= k >> r; 
+      k *= m;
 
-		h *= m;
-		h ^= k;
+      h *= m;
+      h ^= k;
 
-		data += 4;
-		len -= 4;
-	}
+      data += 4;
+      len -= 4;
+    }
 	
-	switch(len)
-	{
-	case 3: h ^= data[2] << 16;
-	case 2: h ^= data[1] << 8;
-	case 1: h ^= data[0];
-	        h *= m;
-	};
+  switch(len)
+    {
+    case 3: h ^= data[2] << 16;
+    case 2: h ^= data[1] << 8;
+    case 1: h ^= data[0];
+      h *= m;
+    };
 
-	h ^= h >> 13;
-	h *= m;
-	h ^= h >> 15;
+  h ^= h >> 13;
+  h *= m;
+  h ^= h >> 15;
 
-	return h;
+  return h;
 } 
 
 VALUE MurmurHashModule = Qnil;
