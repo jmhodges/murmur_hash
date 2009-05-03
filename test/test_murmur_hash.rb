d6b15097da756fd60ea4856556d645fbfa707aa3
Jeff Hodges <jeff@somethingsimilar.com>
2009-05-02 21:52:28 -0700

    Real live tests! Still need a big-endian box to test on. :(

--- test/test_murmur_hash.rb
+++ test/test_murmur_hash.rb
@@ -1,8 +1,44 @@
 require "test/unit"
 require "murmur_hash"
 
+
+# I'm sure there's a simpler way to do this, but I can't remember it
+# sooo. From:
+# http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/256730
+# Also posted at: http://gist.github.com/55220
+module Endian
+  def self.big?
+    endianness == :big
+  end
+
+  def self.little?
+    endianness == :little
+  end
+
+  def self.endianness
+    x = 0xdeadbeef
+
+    endian_type = {
+      Array(x).pack("V*") => :little,
+      Array(x).pack("N*") => :big
+    }
+
+    endian_type[Array(x).pack("L*")]
+  end
+end
+
 class TestMurmurHash < Test::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+  def test_neutral_does_the_right_thing
+    assert_equal 3435905073, MurmurHash.neutral_murmur_hash("string", 23)
+  end
+
+  def test_normal_does_the_right_thing
+    answer = Endian.little? ? 3435905073 : "FIXME find a big-endian box"
+    assert_equal answer, MurmurHash.murmur_hash("string", 23)
+  end
+
+  def test_aligned_does_the_right_thing
+    answer = Endian.little? ? 3435905073 : "FIXME find a big-endian box"
+    assert_equal answer, MurmurHash.murmur_hash("string", 23)
   end
 end
